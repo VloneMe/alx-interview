41adcf8c4e90266207588b4d9088a159a998d17e
VloneMe <francisntima@yahoo.com>
2024-01-05 06:37:47 +0000

    triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,25 +3,22 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
     """
-    Returns a list of lists representing Pascal's Triangle up to the nth row.
+    Returns a list of lists representing Pascal's Triangle up
+    to the nth row, excluding the first row.
     Returns an empty list if n <= 0.
     """
+    triangle = []
     if n <= 0:
-        return []
-
-    triangle = [[1]]  # Initialize Pascal's Triangle with the first row
-
+        return triangle
+    triangle = [[1]]
     for i in range(1, n):
-        row = [1]
+        temp = [1]
         for j in range(len(triangle[i - 1]) - 1):
-            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-        row.append(1)
-        triangle.append(row)
-
+            curr = triangle[i - 1]
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
     return triangle
-
-# Example usage:
-result = pascal_triangle(5)
-print(result)
