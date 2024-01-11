470f31da88ff5b868e39aa286e57ece72fd6ac64
VloneMe <francisntima@yahoo.com>
2024-01-11 08:57:45 +0000

    Lockboxes fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,27 +15,39 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, else False.
     """
-    """ Ensure the input is a list """
+    """ 
+    Ensure the input is a list 
+    """
     if not isinstance(boxes, list):
         return False
 
-    """ Ensure the list is not empty """
+    """ 
+    Ensure the list is not empty 
+    """
     elif not boxes:
         return False
 
-    """ Iterate over the keys """
+    """ 
+    Iterate over the keys
+    """
     for key in range(1, len(boxes) - 1):
         box_checked = False
 
-        """ Check if the key corresponds to a box and is not the same as the box index """
+        """
+        Check if the key corresponds to a box and is not the same as the box index
+        """
         for idx in range(len(boxes)):
             box_checked = key in boxes[idx] and key != idx
             if box_checked:
                 break
 
-        """ If a box couldn't be opened with the current key, return False """
+        """
+        If a box couldn't be opened with the current key, return False
+        """
         if not box_checked:
             return box_checked
 
-    """ If all boxes can be opened, return True """
+    """
+    If all boxes can be opened, return True
+    """
     return True
