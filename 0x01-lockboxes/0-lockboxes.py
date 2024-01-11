b4c9543a2d1e1e4438e8f8f42891decb95850ca6
VloneMe <francisntima@yahoo.com>
2024-01-11 09:12:27 +0000

    Lockboxess

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 
 """
-Lockboxeses
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and may contain keys to other boxes.
+Task: Write a method that determines if all the boxes can be opened.
 """
 
 
 def canUnlockAll(boxes):
     """
     Check if all boxes can be unlocked using their keys.
-    
+
     Args:
         boxes (list): A list of lists representing the boxes and their keys.
 
     Returns:
         bool: True if all boxes can be opened, else False.
     """
-    if not isinstance(boxes, list):
+    # Ensure the input is a list
+    if type(boxes) is not list:
         return False
-    elif not boxes:
+    # Ensure the list is not empty
+    elif len(boxes) == 0:
         return False
+
+    # Iterate over the keys
     for key in range(1, len(boxes) - 1):
         box_checked = False
+
+        # Check if the key corresponds to a box and is not the same as the box index
         for idx in range(len(boxes)):
             box_checked = key in boxes[idx] and key != idx
             if box_checked:
                 break
+
+        # If a box couldn't be opened with the current key, return False
         if not box_checked:
             return box_checked
+
+    # If all boxes can be opened, return True
     return True
