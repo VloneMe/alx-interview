cd8d515d8fac5d3d08c0695007f64cf65b5dccfc
Vlone Me <francisntima@gmail.com>
2024-01-25 13:37:07 +0000

    Fixed: Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,7 +15,8 @@ def print_stats(stats: dict, file_size: int) -> None:
     Print file size and HTTP status code statistics.
 
     Args:
-    - stats (dict): Dictionary containing HTTP status codes and their counts.
+    - stats (dict): Dictionary containing HTTP status codes
+    and their line_counts.
     - file_size (int): Total file size.
 
     Returns:
@@ -29,44 +30,44 @@ def print_stats(stats: dict, file_size: int) -> None:
 
 if __name__ == '__main__':
     # Initialization
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+    file_size, line_count = 0, 0
+    codes_stats = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes_stats}
 
     try:
-        # Reading log data line by line
+        """ Reading log data line by line"""
         for line in sys.stdin:
-            count += 1
+            line_count += 1
             data = line.split()
 
             try:
-                # Extracting status code and updating stats
+                """ Extracting status code and updating stats"""
                 status_code = data[-2]
                 if status_code in stats:
                     stats[status_code] += 1
             except IndexError:
-                # Handling IndexErrors, e.g.,
-                # if the line doesn't have enough elements
+                """ Handling IndexErrors, e.g.,
+                    if the line doesn't have enough elements"""
                 pass
 
             try:
                 # Extracting file size and updating total file size
-                filesize += int(data[-1])
+                file_size += int(data[-1])
             except (IndexError, ValueError):
-                # Handling IndexErrors
-                # (if line doesn't have enough elements) and
-                # ValueErrors (if file size is not an integer)
+                """ Handling IndexErrors
+                    (if line doesn't have enough elements) and
+                    ValueErrors (if file size is not an integer)"""
                 pass
 
-            # Printing stats every 10 lines
-            if count % 10 == 0:
-                print_stats(stats, filesize)
+            """ Printing stats every 10 lines"""
+            if line_count % 10 == 0:
+                print_stats(stats, file_size)
 
-        # Printing final stats
-        print_stats(stats, filesize)
+        """ Printing final stats"""
+        print_stats(stats, file_size)
 
     except KeyboardInterrupt:
-        # Handling KeyboardInterrupt to print stats
-        # before raising the exception
-        print_stats(stats, filesize)
+        """# Handling KeyboardInterrupt to print stats
+             before raising the exception"""
+        print_stats(stats, file_size)
         raise
