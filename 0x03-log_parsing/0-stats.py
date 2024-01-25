594265a3d110d83e9aa3605888c2a25cf5acbc9c
Vlone Me <francisntima@gmail.com>
2024-01-25 12:47:58 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,58 +8,58 @@ HTTP status codes and file sizes, and then prints statistics based on the parsed
 
 import sys
 
-def print_stats(stats: dict, file_size: int) -> None:
+def print_stats(statistics: dict, total_size: int) -> None:
     """
-    Print file size and HTTP status code statistics.
+    Print total file size and HTTP status code statistics.
 
     Args:
-    - stats (dict): Dictionary containing HTTP status codes and their counts.
-    - file_size (int): Total file size.
+    - statistics (dict): Dictionary containing HTTP status codes and their counts.
+    - total_size (int): Total file size.
 
     Returns:
     - None
     """
-    print("File size: {:d}".format(file_size))
-    for k, v in sorted(stats.items()):
-        if v:
-            print("{}: {}".format(k, v))
+    print("Total file size: {:d}".format(total_size))
+    for code, count in sorted(statistics.items()):
+        if count:
+            print("{}: {}".format(code, count))
 
 if __name__ == '__main__':
     # Initialization
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+    total_size, line_count = 0, 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    status_statistics = {code: 0 for code in status_codes}
 
     try:
         # Reading log data line by line
         for line in sys.stdin:
-            count += 1
+            line_count += 1
             data = line.split()
             
             try:
-                # Extracting status code and updating stats
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
+                # Extracting status code and updating status statistics
+                code = data[-2]
+                if code in status_statistics:
+                    status_statistics[code] += 1
             except IndexError:
                 # Handling IndexErrors, e.g., if the line doesn't have enough elements
                 pass
             
             try:
                 # Extracting file size and updating total file size
-                filesize += int(data[-1])
+                total_size += int(data[-1])
             except (IndexError, ValueError):
                 # Handling IndexErrors (if line doesn't have enough elements) and ValueErrors (if file size is not an integer)
                 pass
             
             # Printing stats every 10 lines
-            if count % 10 == 0:
-                print_stats(stats, filesize)
+            if line_count % 10 == 0:
+                print_stats(status_statistics, total_size)
         
         # Printing final stats
-        print_stats(stats, filesize)
+        print_stats(status_statistics, total_size)
 
     except KeyboardInterrupt:
         # Handling KeyboardInterrupt to print stats before raising the exception
-        print_stats(stats, filesize)
+        print_stats(status_statistics, total_size)
         raise
